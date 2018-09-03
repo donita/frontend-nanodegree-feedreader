8e2263982b435520bcacacca47f5651dfeeea920
Donita <mathisdonita@gmail.com>
2018-09-03 13:46:50 -0400

    update feedreader.js.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,16 +78,19 @@ it ('is hidden', function() {
 
 });
 /* TODO: Write a new test suite named "Initial Entries" */
-  describe('Initial Entries', function() {
+  
       /* TODO: Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        * Remember, loadFeed() is asynchronous so this test will require
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
-      beforeEach(function(done) {
-          loadFeed(0, done);
-      });
+      describe('Initial Entries', function(){
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                done();
+            });
+        });
 
       it('completes work', function() {
           const feed = document.querySelector('.feed');
